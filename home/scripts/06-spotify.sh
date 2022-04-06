98533d420f17ecfcb725b7070c607ae71431bd53
Andrea Orru <andrea@orru.io>
2022-04-06 22:41:06 +0900

    Adjust Spotify comment

--- home/scripts/06-spotify.sh
+++ home/scripts/06-spotify.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-# Enable hardware acceleration in Chrome.
+# HiDPI mode for Spotify.
 mkdir -p ~/.local/share/applications && cd ~/.local/share/applications
 cp /usr/share/applications/spotify.desktop .
 sed -i 's/^Exec=spotify/Exec=spotify --force-device-scale-factor=2/' spotify.desktop
