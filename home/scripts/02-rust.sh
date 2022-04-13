19211a3c08d93f6660021411e50f3abc73989a41
Andrea Orru <andreaorru@chromium.org>
2022-04-13 14:03:55 +0900

    Add Rust packages

--- home/scripts/02-rust.sh
+++ home/scripts/02-rust.sh
@@ -1,8 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
+# Install Rust.
 if [ ! -d ~/.cargo ]; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- \
         --default-toolchain nightly --no-modify-path -y
 fi
+
+# Install Rust packages.
+source ~/.cargo/env
+cargo install  \
+    difftastic \
+    git-delta  \
+    lsd
