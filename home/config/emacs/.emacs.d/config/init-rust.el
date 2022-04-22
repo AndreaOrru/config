2b226ac4e709224b2e44258420d05beb7343b3fe
Andrea Orru <andrea@orru.io>
2022-04-22 12:27:58 +0900

    Fix path for Cargo

--- home/config/emacs/.emacs.d/config/init-rust.el
+++ home/config/emacs/.emacs.d/config/init-rust.el
@@ -3,6 +3,7 @@
 (require-package 'rustic)
 
 (after [lsp-mode rustic]
+  (setq exec-path (cons (expand-file-name "~/.cargo/bin") exec-path))
   (setq lsp-rust-analyzer-cargo-watch-command "clippy")
   (setq rustic-format-on-save t))
 
