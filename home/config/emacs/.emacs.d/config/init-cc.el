dd6c53b20b197dc9bd142d07a9b28994c18a8e51
Andrea Orru <andrea@orru.io>
2022-04-22 17:52:33 +0900

    Enable C++ docset in cpp files

--- home/config/emacs/.emacs.d/config/init-cc.el
+++ home/config/emacs/.emacs.d/config/init-cc.el
@@ -4,6 +4,8 @@
 (add-hook 'c-mode-common-hook
           #'(lambda ()
               (add-hook 'before-save-hook 'lsp-format-buffer t t)))
+(add-hook 'c++-mode-hook
+          #'(lambda () (dash-docs-activate-docset "C++")))
 
 (after 'evil-leader
   (evil-leader/set-key-for-mode 'c-mode "o" 'lsp-clangd-find-other-file)
