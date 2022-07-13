15de5386b18df1f5d30bb271473b2d4b815833af
Andrea Orru <andrea@orru.io>
2022-07-14 01:32:22 +0900

    Chrome dark mode

--- home/scripts/05-chrome.sh
+++ home/scripts/05-chrome.sh
@@ -6,4 +6,4 @@ cd $(dirname $(readlink -f $0))
 # Enable hardware acceleration in Chrome.
 mkdir -p ~/.local/share/applications && cd ~/.local/share/applications
 cp /usr/share/applications/google-chrome.desktop .
-sed -i 's/^Exec=\/usr\/bin\/google-chrome-stable/Exec=\/usr\/bin\/google-chrome-stable --enable-features=VaapiVideoDecoder --disable-features=UseChromeOSDirectVideoDecoder --use-gl=desktop --ignore-gpu-blocklist --enable-gpu-rasterization --enable-zero-copy/' google-chrome.desktop
+sed -i 's/^Exec=\/usr\/bin\/google-chrome-stable/Exec=\/usr\/bin\/google-chrome-stable --enable-features=VaapiVideoDecoder --disable-features=UseChromeOSDirectVideoDecoder --use-gl=desktop --ignore-gpu-blocklist --enable-gpu-rasterization --enable-zero-copy --force-dark-mode/' google-chrome.desktop
