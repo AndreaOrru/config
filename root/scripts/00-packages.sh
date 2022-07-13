9d431ee4ce523532dd2493bd8351dc40da8a2c2b
Andrea Orru <andrea@orru.io>
2022-07-14 00:51:21 +0900

    autoremove

--- root/scripts/00-packages.sh
+++ root/scripts/00-packages.sh
@@ -11,9 +11,10 @@ wget -q -O - https://download.spotify.com/debian/pubkey_5E3C45D7B312C643.gpg | s
 echo "deb [arch=amd64] http://emacs.secretsauce.net unstable main" | sudo tee /etc/apt/sources.list.d/emacs.list
 echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
-# Update repositories.
+# Upgrade system.
 sudo apt update
 sudo apt full-upgrade
+sudo apt autoremove
 
 # Install terminal utils.
 sudo apt install -y     \
