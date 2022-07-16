a7d95363c3a4f5108adccf741a7fb82ce563cbfe
Andrea Orru <andrea@orru.io>
2022-07-16 19:07:21 +0900

    init-cc: comments and style

--- home/files/emacs/.emacs.d/config/init-cc.el
+++ home/files/emacs/.emacs.d/config/init-cc.el
@@ -1,13 +1,18 @@
 ;; -*- lexical-binding: t -*-
 
+;; Enable LSP and automatic formatting for C/C++.
 (add-hook 'c-mode-common-hook 'lsp-deferred)
 (add-hook 'c-mode-common-hook
-          #'(lambda ()
+          #'(lambda()
               (add-hook 'before-save-hook 'lsp-format-buffer t t)))
-(add-hook 'c++-mode-hook
-          #'(lambda () (dash-docs-activate-docset "C++")))
 
+;; Enable C++ dash docset.
+(add-hook 'c++-mode-hook
+          #'(lambda() (dash-docs-activate-docset "C++")))
+
+;; Key bindings.
 (after 'evil-leader
+  ;; Switch between implementation and header file.
   (evil-leader/set-key-for-mode 'c-mode "o" 'lsp-clangd-find-other-file)
   (evil-leader/set-key-for-mode 'c++-mode "o" 'lsp-clangd-find-other-file))
 
