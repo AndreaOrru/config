2bec018c63b0e533a9e8b7fb32ecd6cadd987ac9
Andrea Orru <andrea@orru.io>
2022-07-16 19:08:51 +0900

    init-rust: comments

--- home/files/emacs/.emacs.d/config/init-rust.el
+++ home/files/emacs/.emacs.d/config/init-rust.el
@@ -3,8 +3,11 @@
 (require-package 'rustic)
 
 (after [lsp-mode rustic]
+  ;; Add Cargo binaries to the PATH.
   (setq exec-path (cons (expand-file-name "~/.cargo/bin") exec-path))
+  ;; Use clippy for diagnostics.
   (setq lsp-rust-analyzer-cargo-watch-command "clippy")
+  ;; Format on save.
   (setq rustic-format-on-save t))
 
 (provide 'init-rust)
