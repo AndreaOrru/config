51aaac883cbee7e098f406ff3c4f60e4e295b333
Andrea Orru <andrea@orru.io>
2022-07-16 19:09:00 +0900

    init-tree-sitter: comments

--- home/files/emacs/.emacs.d/config/init-tree-sitter.el
+++ home/files/emacs/.emacs.d/config/init-tree-sitter.el
@@ -3,6 +3,7 @@
 (require-package 'tree-sitter)
 (require-package 'tree-sitter-langs)
 
+;; Enable tree-sitter syntax highlighting everywhere.
 (add-hook 'after-init-hook 'global-tree-sitter-mode)
 (add-hook 'tree-sitter-after-on-hook 'tree-sitter-hl-mode)
 
