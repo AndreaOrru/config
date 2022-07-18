1f97cd2dc92176421e20609dc25e2bbc711a1483
Andrea Orru <andrea@orru.io>
2022-07-19 08:33:22 +0900

    Check if Rust is installed before

--- home/scripts/01-rustup.sh
+++ home/scripts/01-rustup.sh
@@ -4,7 +4,9 @@ set -e
 
 # Install Rust using Rustup.
 if [ ! -d ~/.rustup ]; then
-    paru -Rns --noconfirm rust || true
+    if pacman -Q rust &>/dev/null; then
+        paru -Rns --noconfirm rust
+    fi
     paru -S --needed --noconfirm rustup
     rustup self upgrade-data
     rustup default nightly
