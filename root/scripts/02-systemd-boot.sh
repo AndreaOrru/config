4cc6a2b29d5709945705313819c02dbeb1fd3747
Andrea Orru <andrea@orru.io>
2022-07-19 10:00:56 +0900

    systemd-boot-update

--- root/scripts/02-systemd-boot.sh
+++ root/scripts/02-systemd-boot.sh
@@ -3,4 +3,4 @@
 set -e
 
 # Disable timeout (launch Arch directly).
-sudo sed -i 's/timeout 3/timeout 0/' /boot/loader/loader.conf
+sudo sed -i 's/timeout 3$/timeout 0/' /boot/loader/loader.conf
--- root/scripts/04-intel-undervolt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# Enable the undervolt daemon.
-sudo systemctl enable --now intel-undervolt.service
